300aeeb6e3172339a53e1036112a4d6bb400c6ff
mahesh4 <magesh22111995@gmail.com>
2019-11-17 17:00:11 -0700

    Update task3.py

--- Main/Tasks/task3.py
+++ Main/Tasks/task3.py
@@ -1,7 +1,18 @@
+from os.path import join
+
+from Main.HOG_feature_descriptor import HOG
+import pickle
+import Main.config as config
+
 def startTask3():
-    print("Please enter the k value")
-    k = input()
+    print("start task3")
+    k = input("Please enter the k value for outgoing edges ")
+    K = input("Please enter the K value for visualizing dominant images ")
+    hog = HOG()
+    featureVector = hog.HOGFeatureDescriptor()
+    fileHandler = open(join(config.DATABASE_FOLDER, 'hog_features_task_3'), 'wb')
+    pickle.dump(featureVector, fileHandler)
 
 
 if __name__ == '__main__':
-    pass
+    startTask3()
