6ebecc2e749d7e3df01dc748387ed49e82ebb461
Hemanshu Solanki <hemanshu116@gmail.com>
2019-11-19 20:59:45 -0700

    Converting euclidean to probablities

--- Main/Tasks/task3.py
+++ Main/Tasks/task3.py
@@ -51,12 +51,17 @@ def startTask3():
             # print(len(latentFeatureDict[imageNames[i]]), len(latentFeatureDict[imageNames[j]]))
             distances.append(find_distance_2_vectors(latentFeatureDict[imageNames[i]],
                                                      latentFeatureDict[imageNames[j]]))
+
         distances = np.asarray(distances)
         ind = np.argpartition(distances, k)[:k]
-        total = sum(distances[ind])
+        total = 0
+        for distance_index in ind:
+            if distances[distance_index] != 0:
+                total += 1/distances[distance_index]
         for distance_index in ind:
             # This is adding only k nearest neighbours into the matrix and doing ratio to get probablistic matrix
-            adjacency_matrix[i][distance_index] = 1 - distances[distance_index] / total
+            if distances[distance_index] != 0:
+                adjacency_matrix[distance_index][i] = 1/distances[distance_index] / total
 
     rowDict = {}
     i = 0
@@ -68,7 +73,8 @@ def startTask3():
     df.rename(index=rowDict, inplace=True)
 
     df.to_csv(join(config.DATABASE_FOLDER, "adjacency_matrix.csv"))
-    I = np.zeros(df.shape)
+    I = np.identity(df.shape[1])
+    print(I)
     print("Enter the three imageIDs to be used as seed")
     imageID_1 = input()
     imageID_2 = input()
@@ -80,11 +86,11 @@ def startTask3():
     seed.loc[imageID_2] = 0.33
     seed.loc[imageID_3] = 0.34
     print(seed)
-    page_rank = np.matmul(np.linalg.inv(I - .25 * df), 0.75 *seed)
+    page_rank = np.matmul(np.linalg.inv(I - .25 * df), 0.75 * seed)
     print(page_rank.shape)
     steady_state = pd.Series(page_rank, index=df.index)
     steady_state.to_csv(join(config.DATABASE_FOLDER, "steady_state_matrix.csv"))
+
+
 if __name__ == '__main__':
     startTask3()
-
-
