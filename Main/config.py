b5982b29bfec3d6534d61aa65c27cdc37e3852be
varun_sumyag <varun@sumyag.com>
2019-11-30 05:03:03 -0700

    updated config paths

--- Main/config.py
+++ Main/config.py
@@ -1,16 +1,16 @@
 # 11k images folder location.
-FULL_IMAGESET_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\MWDB--11K-Images\images"
+FULL_IMAGESET_FOLDER = "E:\Personal Stuff\Projects\ASU\MWDB\Working_Area\Databases\Images\Hands"
 
 # Image DB Location.
-IMAGE_FOLDER = "H:\Asu\mwdb\project-phase-2\MWDB---Phase--3\Images\Labelled\Set1"
+IMAGE_FOLDER = "E:\Personal Stuff\Projects\ASU\MWDB\Working_Area\Databases\phase3_sample_data\Labelled\Set1"
 
 # Location where feature descriptors/Latent semantics will be stored.
-DATABASE_FOLDER = "H:\Asu\mwdb\project-phase-2\MWDB---Phase--3\Database"
+DATABASE_FOLDER = "E:\Personal Stuff\Projects\ASU\MWDB\Working_Area\Databases\Phase_3"
 
 # Location where images for classification is present
-CLASSIFICATION_FOLDER = "H:\Asu\mwdb\project-phase-2\MWDB---Phase--3\Classify"
+CLASSIFICATION_FOLDER = "E:\Personal Stuff\Projects\ASU\MWDB\Working_Area\Databases\Classify"
 
-VISUALIZATION_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\MWDB--11K-Images\Visualize"
+VISUALIZATION_FOLDER = "E:\Personal Stuff\Projects\ASU\MWDB\Working_Area\Databases\Visualize"
 
 # Path where metadata csv file is present.
-METADATA_FOLDER = "H:\Asu\mwdb\project-phase-2\MWDB---Phase--3\Metadata"
+METADATA_FOLDER = "E:\Personal Stuff\Projects\ASU\MWDB\Working_Area\Databases\Metadata\HandInfo.csv"
