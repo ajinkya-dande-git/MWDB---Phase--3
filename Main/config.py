e848ac6702e0e739212572b9e2f881ea1c25bfb9
Hemanshu Solanki <hemanshu116@gmail.com>
2019-12-02 19:01:25 -0700

    Add folder paths

--- Main/config.py
+++ Main/config.py
@@ -1,16 +1,26 @@
+# Location where images for classification is present
+CLASSIFICATION_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\Phase 3\MWDB---Phase--3\Classify"
+
 # 11k images folder location.
-FULL_IMAGESET_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\MWDB--11K-Images\images"
+FULL_IMAGESET_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\Hands"
 
 # Image DB Location.
-IMAGE_FOLDER = "H:\Asu\mwdb\project-phase-2\MWDB---Phase--3\Images\Labelled\Set1"
+IMAGE_FOLDER_SET_2 = "D:\ASU Projects\CSE 551 - MWDB\Phase 3\MWDB---Phase--3\images\Labelled\Set2"
+IMAGE_FOLDER_SET_1 = "D:\ASU Projects\CSE 551 - MWDB\Phase 3\MWDB---Phase--3\images\Labelled\Set1"
+
+IMAGE_FOLDER_SET_UNLABELLED_1 = "D:\ASU Projects\CSE 551 - MWDB\Phase 3\MWDB---Phase--3\images\\unlabelled\Set 1"
+IMAGE_FOLDER_SET_UNLABELLED_2 = "D:\ASU Projects\CSE 551 - MWDB\Phase 3\MWDB---Phase--3\images\\unlabelled\Set 2"
+
+# Location where outputs.
+DATABASE_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\Phase 3\MWDB---Phase--3\Database"
 
 # Location where feature descriptors/Latent semantics will be stored.
-DATABASE_FOLDER = "H:\Asu\mwdb\project-phase-2\MWDB---Phase--3\Database"
+FEATURES_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\Phase 3\MWDB---Phase--3\Database\Features"
 
-# Location where images for classification is present
-CLASSIFICATION_FOLDER = "H:\Asu\mwdb\project-phase-2\MWDB---Phase--3\Classify"
+FEATURES_FOLDER_LBP = "D:\ASU Projects\CSE 551 - MWDB\Phase 3\MWDB---Phase--3\Database\Features_LBP"
+
 
 VISUALIZATION_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\MWDB--11K-Images\Visualize"
 
 # Path where metadata csv file is present.
-METADATA_FOLDER = "H:\Asu\mwdb\project-phase-2\MWDB---Phase--3\Metadata"
+METADATA_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\Phase 3\MWDB---Phase--3\Metadata\HandInfo.csv"
